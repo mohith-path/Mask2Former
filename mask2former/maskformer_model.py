4b7922eb7b2d1ca68cd2dfb4a8f1ede33a4f522c
Mohith Sakthivel <msakthivel@path-robotics.com>
2024-02-09 12:18:22 -0500

    Make stuff similar to MaskRCNN

--- mask2former/maskformer_model.py
+++ mask2former/maskformer_model.py
@@ -368,13 +368,13 @@ class MaskFormer(nn.Module):
 
         result = Instances(image_size)
         # mask (before sigmoid)
-        result.pred_masks = (mask_pred > 0).float()
+        result.pred_masks = mask_pred.sigmoid().float()
         result.pred_boxes = Boxes(torch.zeros(mask_pred.size(0), 4))
         # Uncomment the following to get boxes from masks (this is slow)
         # result.pred_boxes = BitMasks(mask_pred > 0).get_bounding_boxes()
 
         # calculate average mask prob
         mask_scores_per_image = (mask_pred.sigmoid().flatten(1) * result.pred_masks.flatten(1)).sum(1) / (result.pred_masks.flatten(1).sum(1) + 1e-6)
-        result.scores = scores_per_image * mask_scores_per_image
+        result.scores = scores_per_image
         result.pred_classes = labels_per_image
         return result
